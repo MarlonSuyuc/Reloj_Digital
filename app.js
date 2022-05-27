39d42637e5b8fddf7267c3a38b82ff0fedf0fbe5
marlonsuyuc <marlonsuyuc0@gmail.com>
2022-05-27 17:58:03 -0600

    parte del reloj

---
+++ app.js
@@ -0,0 +1,18 @@
+// Triangulo de asteriscos
+let cad = "";
+for (let fila = 1; fila <= 5; fila++) {
+  for (let columna = 0; columna < fila; columna++) {
+    cad += "* ";
+  }
+//   console.log(cad);
+  cad = "";
+}
+// tabla de multiplicar
+for (let num1 = 1; num1 <= 3; num1++) {
+  for (let num2 = 1; num2 <= 4; num2++) {
+    let resutl = num1 * num2;
+    // console.log(`${num1} * ${num2} = ${resutl}`);
+  }
+//   console.log(`---------`);
+}
+
